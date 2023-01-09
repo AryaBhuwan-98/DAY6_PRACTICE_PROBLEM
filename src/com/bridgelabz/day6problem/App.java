333f375965c0157e04696941ba3157fafee7865a
AryaBhuwan-98 <aryabhuwan873@gmail.com>
2023-01-09 13:25:13 -0800

    Add:Welcome Message to DAY6 JAVA PROGRAM

---
+++ src/com/bridgelabz/day6problem/App.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.day6problem;
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Welcome To DAy6 JAVA PROGRAM");
+
+    }
+}
